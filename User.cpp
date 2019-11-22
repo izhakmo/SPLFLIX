83d9e7ab6f4216e72aefd5d7f92122c59139c154
ziv232 <58069685+ziv232@users.noreply.github.com>
2019-11-22 17:58:21 +0200

    Update User.cpp

--- User.cpp
+++ User.cpp
@@ -3,5 +3,19 @@
 //
 // Created by izhakmo on 20/11/2019.
 User::User(const std::string &name) :name(name),historynew vector<Watchable*>(){}
-//
+
+
+std::vector<Watchable*> User::get_history() const {
+    return history;
+}
+
+const std::string & User::getName() const {
+    return name;
+}
+
+//========LengthRecommenderUser============
+LengthRecommenderUser::LengthRecommenderUser(const std::string &name) : User(name), numSeen(0) {
+
+}
+
 
