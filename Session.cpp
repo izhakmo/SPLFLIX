429ab8f8393ce8e4429841d1afa2022d45750520
ziv232 <58069685+ziv232@users.noreply.github.com>
2019-11-22 18:21:58 +0200

    Update Session.cpp

--- Session.cpp
+++ Session.cpp
@@ -8,7 +8,23 @@ std::ifstream i("config1.json");
 json j;
 i>>j;
 
+unordered_map<string, User *>& Session::getUserMap() {
+    return userMap;
+}
 
+User*Session::searchUser(string name) {
+    unordered_map<std::string,User*> tempMap = getUserMap();
+    User* userPtr;
+    unordered_map<std::string,User*>::const_iterator got = tempMap.find (name);
+    if(got==tempMap.end()){
+        userPtr = nullptr;
+    }
+    else{
+        userPtr = got->second;
+    }
+
+    return userPtr;
+}
 
 
 //
