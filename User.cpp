9f46d3ee834d322c5b3f2b3c3f8b21cbb11ab1f4
izhakmo <58077641+izhakmo@users.noreply.github.com>
2019-11-23 16:37:06 +0200

    Update User.cpp

--- User.cpp
+++ User.cpp
@@ -1,10 +1,12 @@
 #include "User.h"
+#include "Watchable.h"
 #include <queue>
 
 //
 // Created by izhakmo on 20/11/2019.
 using namespace std;
 User::User(const std::string &name) :name(name),history(vector<Watchable*>()){}
+User::~User() {history.clear();}    //TODO check the Watchable memory release
 
 
 std::vector<Watchable*> User::get_history() const {
@@ -16,7 +18,9 @@ string User::getName() const {
 }
 
 //========LengthRecommenderUser============
-LengthRecommenderUser::LengthRecommenderUser(const std::string &name) : User(name), numSeen(0),average(0) {
+LengthRecommenderUser::LengthRecommenderUser(const std::string &name) : User(name), numSeen(0),average(0) { }
+
+Watchable* LengthRecommenderUser::getRecommendation(Session &s) {
 
 }
 
@@ -24,4 +28,17 @@ LengthRecommenderUser::LengthRecommenderUser(const std::string &name) : User(nam
 RerunRecommenderUser::RerunRecommenderUser(const std::string& name)
 :recommendations(queue<Watchable*>()),User(name){}
 
+Watchable* RerunRecommenderUser::getRecommendation(Session &s) {    //if the Q is empty we return null, else, dequeue and enqueue the element
+    if(recommendations.empty())
+        return nullptr;     //TODO CHECK VIRTUAL
+    else{
+        Watchable *rec=recommendations.front();
+        recommendations.pop();
+        recommendations.push(rec);
+        return rec;
+    }
+}
+
 //===========GenreRecommenderUser===========
+GenreRecommenderUser::GenreRecommenderUser(const std::string& name)
+:User(name){}
